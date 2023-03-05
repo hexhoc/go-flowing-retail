bca0371e46ba35c15fe3bbcfbaf8feada39f90be
hexhoc <hexhoc@gmail.com>
2023-03-05 23:26:03 +0300

    Add implementation of auth service func

--- microservices/auth-service/internal/services/auth.go
+++ microservices/auth-service/internal/services/auth.go
@@ -2,32 +2,87 @@ package services
 
 import (
 	"context"
+	"github.com/hexhoc/auth-service/internal/db"
+	"github.com/hexhoc/auth-service/internal/models"
 	"github.com/hexhoc/auth-service/internal/pb"
+	"github.com/hexhoc/auth-service/internal/utils"
+	"net/http"
 )
 
 type Server struct {
+	H   db.Handler
+	Jwt utils.JwtWrapper
 }
 
 func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	var user models.User
+	result := s.H.DB.Where(&models.User{Email: req.Email}).First(&user)
+	if result.Error == nil {
+		return &pb.RegisterResponse{
+			Status: http.StatusConflict,
+			Error:  "E-Mail already exists",
+		}, nil
+	}
+
+	user.Email = req.Email
+	user.Password = utils.HashPassword(req.Password)
+	s.H.DB.Create(&user)
 
 	return &pb.RegisterResponse{
-		Status: "ok",
-		Error:  "",
+		Status: http.StatusCreated,
 	}, nil
 }
 
 func (s *Server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	var user models.User
+
+	result := s.H.DB.Where(&models.User{Email: req.Email}).First(&user)
+	if result.Error != nil {
+		return &pb.LoginResponse{
+			Status: http.StatusNotFound,
+			Error:  "User not found",
+		}, nil
+	}
+
+	match := utils.CheckPasswordHash(req.Password, user.Password)
+
+	if !match {
+		return &pb.LoginResponse{
+			Status: http.StatusNotFound,
+			Error:  "User not found",
+		}, nil
+	}
+
+	token, _ := s.Jwt.GenerateToken(user)
+
 	return &pb.LoginResponse{
-		Status: "ok",
-		Error:  "",
+		Status: http.StatusOK,
+		Token:  token,
 	}, nil
 }
 
 func (s *Server) Validate(ctx context.Context, req *pb.ValidateRequest) (*pb.ValidateResponse, error) {
 
+	claims, err := s.Jwt.ValidateToken(req.Token)
+
+	if err != nil {
+		return &pb.ValidateResponse{
+			Status: http.StatusBadRequest,
+			Error:  err.Error(),
+		}, nil
+	}
+
+	var user models.User
+
+	if result := s.H.DB.Where(&models.User{Email: claims.Email}).First(&user); result.Error != nil {
+		return &pb.ValidateResponse{
+			Status: http.StatusNotFound,
+			Error:  "User not found",
+		}, nil
+	}
+
 	return &pb.ValidateResponse{
-		Status: 0,
-		Error:  "",
-		UserId: 0,
+		Status: http.StatusOK,
+		UserId: user.Id,
 	}, nil
 }
